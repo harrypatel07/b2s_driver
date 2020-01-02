72a2bdae85c75c3f8d8adb63aef89688a68366fd
ts24corp <play@ts24.com.vn>
2020-01-02 09:01:32 +0700

    up

---
+++ lib/src/app/service/bluetooh-barcode-service.dart
@@ -0,0 +1,7 @@
+class BluetoothBarcodeService {
+  static listenValueQrCode() async {
+    // List<BluetoothService> services = await device.discoverServices();
+    // FlutterBlue flutterBlue = FlutterBlue.instance;
+    // var a = await flutterBlue.connectedDevices;
+  }
+}

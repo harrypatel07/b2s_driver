2b73da44040db267fe35208c02669e4ff8c53611
ts24corp <play@ts24.com.vn>
2020-06-01 17:03:00 +0700

    up

--- lib/src/app/core/app_setting.dart
+++ lib/src/app/core/app_setting.dart
@@ -37,7 +37,7 @@ const emptyState = {
   'assetImage': AssetImage('assets/images/empty.png'),
   'assetPath': 'assets/images/empty.png',
 };
-const version = "1.0.26";
+const version = "1.0.27";
 
 Api api = new Api();
 
--- lib/src/app/provider/api_parent_01.dart
+++ lib/src/app/provider/api_parent_01.dart
@@ -745,12 +745,12 @@ class Api1 extends ApiMaster {
     if (date == null) date = DateFormat('yyyy-MM-dd').format(DateTime.now());
     List<DriverBusSession> listResult = new List();
     body = new Map();
-//    body["vehicle_id"] = vehicleId;
-//    body["driver_id"] = driverId;
-//    body["date"] = date;
-     body["vehicle_id"] = 116;
-     body["driver_id"] = 4609;
-     body["date"] = "2020-06-01";
+    body["vehicle_id"] = vehicleId;
+    body["driver_id"] = driverId;
+    body["date"] = date;
+    // body["vehicle_id"] = 116;
+    // body["driver_id"] = 4609;
+    // body["date"] = "2020-06-01";
     print(body);
     return client
         .callController("/handle_picking_info_request_v2", body)
